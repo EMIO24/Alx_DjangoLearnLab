d6a100385c729d05f8173db87235a12abe6aeb6b
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-16 19:34:56 +0100

    django

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'EMIO24',
+        'PASSWORD': 'Passion13.@1',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
