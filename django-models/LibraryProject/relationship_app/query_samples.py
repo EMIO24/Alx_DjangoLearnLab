e282c2548628ca64003157efc0ce57202b80e46a
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-19 15:15:10 +0100

    A_django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,23 @@
+import django
+import os
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_books_by_author(author_name):
+    """Query all books by a specific author."""
+    books = Book.objects.filter(author__name=author_name)
+    return books
+
+def query_books_in_library(library_name):
+    """List all books in a specific library."""
+    books = Book.objects.filter(library__name=library_name)
+    return books
+
+def query_librarian_for_library(library_name):
+    """Retrieve the librarian for a specific library."""
+    librarian = Librarian.objects.filter(library__name=library_name).first()
+    return librarian
