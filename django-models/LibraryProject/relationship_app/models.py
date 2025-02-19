321684cd85808065bfe0d1feec8c4410675059bc
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-19 15:38:18 +0100

    A_django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -18,5 +18,5 @@ class Library(models.Model):
     
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete= models.CASCADE)
+    library = models.OneToOneField(Library, on_delete= models.CASCADE, related_name='librarian')
     
