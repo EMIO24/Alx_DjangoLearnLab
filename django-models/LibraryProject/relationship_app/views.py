021cfbafa0acbbb44169d8f17a9ba2a8b0b06cdb
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-20 08:41:02 +0100

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,10 +3,8 @@ from django.views.generic import DetailView
 from .models import Book
 
 def book_list(request):
-      """Retrieves all books and renders a template displaying the list."""
-      books = Book.objects.all()  # Fetch all book instances from the database
-      context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'books/book_list.html', context)
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
   
 class LibraryDetailView(DetailView):
     """
