349cb92119807993e3b5a8bb0fffc57fd8b47c7f
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-23 21:18:03 +0100

    user_A

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library
