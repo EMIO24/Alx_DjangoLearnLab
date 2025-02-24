6871cb6af5b463a3a51dffef72ca12648d0c8a0a
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-24 08:18:37 +0100

    role

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -66,12 +66,10 @@ def user_login(request):
     
     return render(request, "login.html")
 
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-
-def Admin(user):
-    return user.userprofile.role == 'Admin'
-
-@user_passes_test(Admin)
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_page.html')
 
