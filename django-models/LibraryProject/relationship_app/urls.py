67b1d85ad0bdb9d260c778da011c77322a5dbd64
EMIO24 <emmanuel.osarodion@icloud.com>
2025-02-24 07:35:52 +0100

    role

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views
 from .views import register
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 from . import views
 
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('books/', list_books, name = 'book-list'),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),  # Login
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),  # Logout
+    path('admin_page/', admin_view, name='admin_view'),
+    path('librarian_page/', librarian_view, name='librarian_view'),
+    path('member_page/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 from .models import Library
 from .models import Book
 
@@ -80,3 +81,23 @@ def register(request):
 
     return render(request, "register.html", {"form": form})
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_page.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_page.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_page.html')
