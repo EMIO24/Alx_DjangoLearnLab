d8d12bdfbeb04716fa08bb01fafe7e7f5a09c8ee
EMIO24 <emmanuel.osarodion@icloud.com>
2025-03-17 13:39:40 +0100

    api_project

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'advanced_api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'EMIO24',
+        'PASSWORD': 'Passion13.@1',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
