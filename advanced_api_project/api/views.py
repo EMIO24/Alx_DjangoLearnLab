a65fc8a2f825893fba8f7533e1c54b4b5613886c
EMIO24 <emmanuel.osarodion@icloud.com>
2025-03-18 22:07:52 +0100

    custom_api

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# custom views
+from rest_framework import generics
+from rest_framework import permissions
+from .models import Book
+from .serializers import BookSerializer
 
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] # Allow read access to all
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated] # Only authenticated users can create
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 # Create your views here.
