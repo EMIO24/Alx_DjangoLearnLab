f9c3812ed3e5acb6810d0a863750a99502ad70ff
EMIO24 <emmanuel.osarodion@icloud.com>
2025-03-19 08:34:59 +0100

    api

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,15 +1,16 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+# Represents an author with a name.
 class Author(models.Model):
-    """Represents an author with a name."""
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
+# Represents a book with a title, publication year, and author.
 class Book(models.Model):
-    """Represents a book with a title, publication year, and author."""
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
