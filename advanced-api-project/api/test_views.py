2e153a8fcd0c3aad1ff630c9dfc8e0460b821d32
EMIO24 <emmanuel.osarodion@icloud.com>
2025-03-24 16:54:46 +0100

    test

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,21 +1,20 @@
+'''
 # api/test_views.py
-from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 from django.urls import reverse
-from api.models import Book  # Assuming your Book model is in api/models.py
+from .models import Book  # Assuming your Book model is here
+from django.contrib.auth.models import User  # For authentication tests
 
-class BookCreateTest(TestCase):
+class BookCreateTest(APITestCase):
     def setUp(self):
-        # This runs before each test method
-        self.client = APIClient()
-        self.create_url = reverse('book-list')  # Assuming you have a URL named 'book-list' for creating books
-
-    def test_can_create_book(self):
+        self.create_url = reverse('book-list') # Assuming you have a URL named 'book-list'
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        
+def test_can_create_book(self):
         # What data are we going to send to create a book?
-        book_data = {'title': 'The Hitchhiker\'s Guide to the Galaxy', 'author': 'Douglas Adams', 'publication_date': '1979-10-12'}
-
+        book_data = {'title': 'The Lord of the Rings', 'author': 'J.R.R. Tolkien', 'publication_date': '1954-07-29'}
         # Simulate making a POST request to our create book endpoint
-        response = self.client.post(self.create_url, book_data, format='json')
+        response = self.client.post(self.create_url, book_data, format='json') # Note the 'format' argument
 
         # Now we check if the response is what we expect
 
@@ -26,6 +25,60 @@ class BookCreateTest(TestCase):
         self.assertEqual(Book.objects.count(), 1)
 
         # Check if the data in the response matches what we sent
-        self.assertEqual(response.data['title'], 'The Hitchhiker\'s Guide to the Galaxy')
-        self.assertEqual(response.data['author'], 'Douglas Adams')
-        self.assertEqual(response.data['publication_date'], '1979-10-12')
+        self.assertEqual(response.data['title'], 'The Lord of the Rings')
+def test_create_book_with_authentication(self):
+        self.client.force_authenticate(user=self.user) # Simulate being logged in as 'testuser'
+        book_data = {'title': 'Pride and Prejudice', 'author': 'Jane Austen', 'publication_date': '1813-01-28'}
+        response = self.client.post(self.create_url, book_data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(Book.objects.first().created_by, self.user) # Assuming you have a 'created_by' field
+        self.client.force_authenticate(user=None) # Remove authentication for subsequent tests if needed '''
+# api/test_views.py
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Book
+from django.contrib.auth.models import User
+
+class BookAPITests(APITestCase):
+    def setUp(self):
+        self.list_create_url = reverse('book-list') # URL for listing and creating books
+        self.detail_url_pattern = reverse('book-detail', kwargs={'pk': 1}) # URL pattern for individual book details
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.book1 = Book.objects.create(title='Book 1', author='Author A', publication_date='2023-01-01')
+        self.book2 = Book.objects.create(title='Book 2', author='Author B', publication_date='2024-02-02')
+
+    def test_can_create_book(self):
+        book_data = {'title': 'New Book', 'author': 'New Author', 'publication_date': '2025-03-03'}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.list_create_url, book_data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), 3)
+        self.assertEqual(response.data['title'], 'New Book')
+
+    def test_can_list_books(self):
+        response = self.client.get(self.list_create_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2) # Assuming no pagination
+
+    def test_can_retrieve_book(self):
+        detail_url = reverse('book-detail', kwargs={'pk': self.book1.pk})
+        response = self.client.get(detail_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], 'Book 1')
+
+    def test_can_update_book(self):
+        detail_url = reverse('book-detail', kwargs={'pk': self.book1.pk})
+        updated_data = {'title': 'Updated Book 1'}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(detail_url, updated_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.title, 'Updated Book 1')
+
+    def test_can_delete_book(self):
+        detail_url = reverse('book-detail', kwargs={'pk': self.book2.pk})
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(detail_url)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Book.objects.count(), 1)
